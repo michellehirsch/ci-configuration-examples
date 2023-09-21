116bb21d091115a63d95f1bf28246ade7e170960
Michelle Hirsch <mhirsch@mathworks.com>
2023-09-21 19:10:50 -0400

    I tried to improve rightTri.

--- code/rightTri.m
+++ code/rightTri.m
@@ -1,10 +1,12 @@
 function angles = rightTri(sides)
 % rightTri Compute the angles of a right triangle
 %
-% angles = rightTri([side1 side2]) returns array angles containing the the
-% three angles of right triangle with side lengths side1 and side2
+% ANGLES = rightTri(SIDES) returns array ARRAY containing the the
+% three angles of a right triangle with side lengths SIDES. SIDES is a
+% 2-element vector specifying the lengths of the two sides joined by the
+% right angle. 
 
-A = atand(sides(1)/sides(2));
+A = atan(sides(1)/sides(2));
 B = atand(sides(2)/sides(1));
 hypotenuse = sides(1)/sind(A);
 C = asind(hypotenuse*sind(A)/sides(1));
