5283df0455e272adbfc11aff61a688fcec47e110
Michelle Hirsch <mhirsch@mathworks.com>
2023-09-21 12:38:54 -0400

    Added help to rightTri

--- code/rightTri.m
+++ code/rightTri.m
@@ -1,4 +1,8 @@
 function angles = rightTri(sides)
+% rightTri Compute the angles of a right triangle
+%
+% angles = rightTri([side1 side2]) returns array angles containing the the
+% three angles of right triangle with side lengths side1 and side2
 
 A = atand(sides(1)/sides(2));
 B = atand(sides(2)/sides(1));
