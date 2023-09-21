0b034e244faba0fc3070f0862e290c1e73227402
Michelle Hirsch <mhirsch@mathworks.com>
2023-09-21 17:47:29 -0400

    More test cleanup.

--- tests/rightTriTest.m
+++ tests/rightTriTest.m
@@ -1,32 +1,22 @@
 % test triangles
 tri = [7 9];
-triIso = [4 4];
-tri306090 = [2 2*sqrt(3)];
+triIsosceles = [4 4];
 
-% preconditions
+% preconditions. 3rd angle must be 90°
 angles = rightTri(tri);
 assert(angles(3) == 90,'Fundamental problem: rightTri not producing right triangle')
 
 %% Test 1: sum of angles
+% Must be 180°
 angles = rightTri(tri);
 assert(sum(angles) == 180)
  
-angles = rightTri(triIso);
+angles = rightTri(triIsosceles);
 assert(sum(angles) == 180)
  
-angles = rightTri(tri306090);
-assert(sum(angles) == 180)
- 
-angles = rightTri(triSkewed);
-assert(sum(angles) == 180)
-
 %% Test 2: isosceles triangles
-angles = rightTri(triIso);
+% Angles must be 45°
+angles = rightTri(triIsosceles);
 assert(angles(1) == 45)
 assert(angles(1) == angles(2))
  
-%% Test 3: 30-60-90 triangle
-angles = rightTri(tri306090);
-assert(abs(angles(1)-30) <= tol)
-assert(abs(angles(2)-60) <= tol)
-assert(abs(angles(3)-90) <= tol)
